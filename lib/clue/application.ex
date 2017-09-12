f3aa4a581829ae743265229be273f27a7b16ca91
James Tortorelli <james.tortorelli@metova.com>
2017-09-12 15:41:44 -0500

    Remove IO statement from application

--- lib/clue/application.ex
+++ lib/clue/application.ex
@@ -2,8 +2,6 @@ defmodule Clue.Application do
   use Application
 
   def start(_type, _args) do
-    result = Clue.Supervisor.start_link
-    IO.puts "application started"
-    result
+    Clue.Supervisor.start_link
   end
 end
